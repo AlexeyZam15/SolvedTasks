97f0cee6f4c6ecc5fb2f689fdbe412871d7f007f
Alexey Zamaraev <www.wolfy.ky@mail.ru>
2022-11-10 21:28:22 +1000

    Solved task1

---
+++ task1/Program.cs
@@ -0,0 +1,7 @@
+﻿// Определить 4 случайных числа и вывести их в консоль
+int a = new Random().Next(0, 100);
+int b = new Random().Next(0, 100);
+int c = new Random().Next(0, 100);
+int d = new Random().Next(0, 100);
+// Console.WriteLine("Решение задачи: "+a+" "+b+" "+c+" "+d);
+Console.WriteLine($"Решение задачи: {a} {b} {c} {d}");
