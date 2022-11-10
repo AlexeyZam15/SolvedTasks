63d20b5a298732fe0537a3f6894f0eb3a9c1679c
Alexey Zamaraev <www.wolfy.ky@mail.ru>
2022-11-10 21:31:45 +1000

    Solved task5

---
+++ task5/Program.cs
@@ -0,0 +1,12 @@
+﻿// Даны два действительных числа x и у. Вычислить их сумму, разность, произведение и частное 
+double x = new Random().NextDouble();
+double y = new Random().NextDouble();
+double sum = x + y;
+double raz = x - y;
+double umn = x * y;
+double del = x / y;
+Console.WriteLine($"Какие числа мы вычисляем: x = {x}, y = {y}");
+Console.WriteLine($"Сумма чисел {sum}");
+Console.WriteLine($"Разница чисел {raz}");
+Console.WriteLine($"Произведение чисел {umn}");
+Console.WriteLine($"Частное чисел {del}");
